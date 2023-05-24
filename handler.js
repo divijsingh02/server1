8355dc29ffc8e2f5552a9b0c23dd15ea4bbf68dc
Divij Singh <divij.singhuipath.com@Divijs-MacBook-Pro.local>
2023-05-24 10:30:20 +0530

    3rd commit

--- handler.js
+++ handler.js
@@ -9,9 +9,12 @@ exports.handler = async (event) => {
   };
   
   function calculateFactorial(n) {
-    if (n === 0 || n === 1) {
-      return 1;
-    } else {
-      return n * calculateFactorial(n - 1);
+    let factorial = 1;
+    
+    for (let i = 2; i <= n; i++) {
+      factorial *= i;
     }
+    
+    return factorial;
   }
+  
