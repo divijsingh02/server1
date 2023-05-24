5f30b710c6d833e8f42c6986d060a9abc2b1323a
Divij Singh <divij.singhuipath.com@Divijs-MacBook-Pro.local>
2023-05-24 10:26:38 +0530

    new commit

--- handler.js
+++ handler.js
@@ -1,21 +1,17 @@
 exports.handler = async (event) => {
-    const n = parseInt(event.n);
-  
-    if (isNaN(n) || n < 0) {
-      return {
-        statusCode: 400,
-        body: 'Invalid input. Please provide a non-negative integer.',
-      };
-    }
-  
-    let factorial = 1;
-    for (let i = 1; i <= n; i++) {
-      factorial *= i;
-    }
-  
+    const n = event.n;
+    const factorial = calculateFactorial(n);
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ factorial }),
+      body: JSON.stringify({ factorial })
     };
   };
   
+  function calculateFactorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * calculateFactorial(n - 1);
+    }
+  }
